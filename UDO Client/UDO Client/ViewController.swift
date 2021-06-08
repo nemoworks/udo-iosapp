ed4764fed5b69746de109b3307a14dd03cdac166
Cui Zihan <zihancui@smail.nju.edu.cn>
2021-06-08 15:50:44 +0800

    init project

---
+++ UDO Client/UDO Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDO Client
+//
+//  Created by 崔子寒 on 2021/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
